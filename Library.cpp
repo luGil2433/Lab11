f4e894ed4e72392643ca0739038f31b40e9e1445
LuCodes33 <151774820+LuCodes33@users.noreply.github.com>
2023-11-30 13:18:49 -0500

    I think I get how this works
    I dont really understand visual studio do

--- Library.cpp
+++ Library.cpp
@@ -6,7 +6,12 @@ using namespace std;
 
 
 //implement class functions
-//Using the provided header file Library.h, write a library class. The class has functions to add a book, remove a book, and print all books. The library can hold at most 10 books. Refer to the header file for the implementation details. Write your class implementation in the file Library.cpp. You also need to write a program that creates a Library object and prompts for one of the following commands: a bookName //add the book named bookName to the library if not already existing and //if there is availability r bookName //remove the book bookName if it exists in the library p //print the library contents q //quit the program Your program should show messages if it cannot successfully add or remove a book. You can assume that the input command is always valid, and the book name always consists of a single word.
+//Using the provided header file Library.h, write a library class. The class has functions to add a book, remove a book, and print all books. 
+//The library can hold at most 10 books. Refer to the header file for the implementation details. Write your class implementation in the file Library.cpp. 
+//You also need to write a program that creates a Library object and prompts for one of the following commands: a bookName 
+//add the book named bookName to the library if not already existing and //if there is availability r bookName //remove the book bookName if it exists in the library p 
+//print the library contents q //quit the program Your program should show messages if it cannot successfully add or remove a book. You can assume that the input command is 
+//always valid, and the book name always consists of a single word.
 
 //constructor
 //creates a library with no books
